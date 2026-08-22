292f6302d498cb3d77a20449b863b7d610895a15
Thiago Assis <thiago.assisfernandes@gmail.com>
2026-08-22 20:47:27 -0300

    done

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-  // Print the greeting below.
-  printf("Hello, C!\n");
+  int num1 = 0;
+  int num2 = 0;
+
+  scanf("%d", &num1);
+  scanf("%d", &num2);
+  // Print a + b using printf.
+  printf("%d\n", num1 + num2);
   return 0;
 }
