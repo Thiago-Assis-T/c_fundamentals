4b16c673180b4b245cccbf09ca17459de5dea01e
Thiago Assis <thiago.assisfernandes@gmail.com>
2026-08-22 22:48:39 -0300

    done

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 int main(void) {
   int nums[5] = {0};
-  int max;
   for (int i = 0; i < 5; i++) {
     scanf("%d", &nums[i]);
   }
 
+  int max = nums[0];
   for (int i = 0; i < 5; i++) {
     if (max < nums[i]) {
       max = nums[i];
