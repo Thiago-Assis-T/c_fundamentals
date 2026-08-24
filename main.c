3f185517782161d1c3cc418a205513c13d382129
Thiago Assis <thiago.assisfernandes@gmail.com>
2026-08-24 18:55:52 -0300

    done

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-  int nums[5] = {0};
-  for (int i = 0; i < 5; i++) {
-    scanf("%d", &nums[i]);
-  }
+  char buf[100];
+  long long stringLength = 0;
+  fgets(buf, sizeof buf, stdin);
 
-  int max = nums[0];
-  for (int i = 0; i < 5; i++) {
-    if (max < nums[i]) {
-      max = nums[i];
-    }
-  }
+  stringLength = strlen(buf);
 
-  printf("%d", max);
+  printf("%lld", stringLength - 1);
+  // Strip trailing newline if present, then print length.
   return 0;
 }
