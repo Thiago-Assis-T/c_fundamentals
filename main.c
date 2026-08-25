0fafc4391bafff72371cb279e35fcf889847d4dc
Thiago Assis <thiago.assisfernandes@gmail.com>
2026-08-24 22:01:18 -0300

    done

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int static square(int n) { return n * n; }
+void static swap(int *pointerA, int *pointerB) {
+  int temp = *pointerB;
+  *pointerB = *pointerA;
+  *pointerA = temp;
+  // Swap the values pointed to by a and b.
+}
 
 int main(void) {
-  int num;
-  scanf("%d", &num);
-  printf("%d\n", square(num));
+  int num1 = 0;
+  int num2 = 0;
+  scanf("%d %d", &num1, &num2);
+  swap(&num1, &num2);
+  printf("%d %d\n", num1, num2);
   return 0;
 }
