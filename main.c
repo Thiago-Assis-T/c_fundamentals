57cafa559f3581f6400890629b6bc8051a0cea12
Thiago Assis <thiago.assisfernandes@gmail.com>
2026-08-27 21:32:03 -0300

    done

--- main.c
+++ main.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int static doSum(int const *nums, int const size) {
-  int sum = 0;
-
-#pragma unroll 5
-  for (int i = 0; i < size; i++) {
-    sum += nums[i];
-  }
-
-  return sum;
-}
+#include <string.h>
 
 int main(void) {
-  int size = 0;
-
-  scanf("%d", &size);
-
-  int *nums =
-      malloc(size * sizeof(int)); /* n ints' worth of bytes, from the heap */
-  if (nums == NULL) {             /* allocation CAN fail */
-    return 1;
+  int num = 0;
+  int count = 0;
+  scanf("%d", &num);
+  FILE *out = fopen("/tmp/clines.txt", "we");
+  if (out == NULL) {
+    return EXIT_FAILURE;
   }
 
+  char buf[1024];
 #pragma unroll 5
-  for (int i = 0; i < size; i++) {
-    scanf("%d", &nums[i]); /* index it exactly like an array */
+  for (int i = 0; i < num; i++) {
+    if (fgets(buf, sizeof(buf), stdin) != NULL) {
+      fputs(buf, out);
+      // Ensure a trailing newline if missing
+      size_t len = strlen(buf);
+      if (len > 0 && buf[len - 1] != '\n')
+        fputc('\n', out);
+    }
   }
 
-  // for (int i = 0; i < size; i++) {
-  //   printf("%d\n", nums[i]); /* index it exactly like an array */
-  // }
+  int file_close_status = fclose(out);
+  if (file_close_status != EXIT_SUCCESS) {
+    perror("Error closing the file");
+    return file_close_status;
+  }
 
-  int sum = doSum(nums, size);
-  printf("%d\n", sum);
+  FILE *input = fopen("/tmp/clines.txt", "re");
+  if (input == NULL) {
+    return EXIT_FAILURE;
+  }
 
-  free(nums); /* hand the bytes back */
-  return 0;
+  while (fgets(buf, sizeof(buf), input) != NULL) {
+    count++;
+  }
+
+  file_close_status = fclose(input);
+  if (file_close_status != EXIT_SUCCESS) {
+    perror("Error closing the file");
+    return file_close_status;
+  }
+
+  printf("lines: %d\n", count);
+
+  return EXIT_SUCCESS;
 }
